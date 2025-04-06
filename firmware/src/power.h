0ae52bda2332a8467417be31c0d7c8dc50dac923
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-04-06 15:50:22 +0200

    use pullup-resistors as transistor resistor

--- firmware/src/power.h
+++ firmware/src/power.h
@@ -1,4 +1,8 @@
 #include "pins.h"
 void set_power(bool state) {
-    digitalWrite(PIN_PWR_EN, state);
+    if (state) pinMode(PIN_PWR_EN, INPUT_PULLUP); //use built-in resistor as current limiting
+    else {
+        digitalWrite(PIN_PWR_EN, LOW);
+        pinMode(PIN_PWR_EN, OUTPUT);
+    }
 }
