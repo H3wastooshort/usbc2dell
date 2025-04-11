2db757045a53fc9f991145465319ae05b268efa7
H3wastooshort <29732082+H3wastooshort@users.noreply.github.com>
2025-04-11 10:10:00 +0200

    LED dimming

--- firmware/src/led.h
+++ firmware/src/led.h
@@ -10,5 +10,6 @@ constexpr uint8_t LED_COLOR_20V[3] = {0x78, 0x5e, 0xf0};
 constexpr uint8_t LED_COLOR_PPS[3] = {0x64, 0x8f, 0xff};
 
 void set_led_color(const uint8_t color[3]) {
-    for (uint8_t i=0; i<3; i++)analogWrite(PINS_LED[i],color[i]);
+    uint8_t dim = (!digitalRead(PIN_SW_LED_BRIGHT)) ? 4 : 8; //divide by 4, if dimming enabled
+    for (uint8_t i=0; i<3; i++)analogWrite(PINS_LED[i], color[i] / dim);
 }
